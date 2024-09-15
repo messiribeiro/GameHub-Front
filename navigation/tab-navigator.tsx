27b4f39963453569e257eb83e334b5dafbff7d09
Luc4sM0ur4 <lucasmoura112.lm@gmail.com>
2024-09-15 16:10:02 -0300

    Atualização feitas: Recomendações, topo da tela inicial

--- navigation/tab-navigator.tsx
+++ navigation/tab-navigator.tsx
@@ -6,6 +6,9 @@ import { HeaderButton } from '../components/HeaderButton';
 import { TabBarIcon } from '../components/TabBarIcon';
 import One from '../screens/one';
 import Two from '../screens/two';
+import TelaInicial from '../screens/telainicial'
+import Login from '../screens/login';
+import RecomendacaoUser from '../screens/recomendacaouser';
 
 const Tab = createBottomTabNavigator();
 
@@ -32,6 +35,30 @@ export default function TabLayout({ navigation }: Props) {
         options={{
           title: 'Tab Two',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
+       <Tab.Screen
+                     name="Login"
+                     component={Login} //
+                     options={{
+                       title: 'Login',
+                       tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
+                     }}
+                   />
+      <Tab.Screen
+        name="TelaInicial"
+        component={TelaInicial} //
+        options={{
+            title: 'Tela Inicial',
+                tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
+              }}
+            />
+      <Tab.Screen
+        name="RecomendacaoUser"
+        component={RecomendacaoUser} //
+        options={{
+          title: 'Recomendação',
+          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
         }}
       />
     </Tab.Navigator>
