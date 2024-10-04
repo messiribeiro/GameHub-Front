501960e713324685b193bb3515b934b88e7873e3
messiribeiro <micaelribeirodossantos@gmail.com>
2024-10-04 02:50:33 -0300

    comecei implementar a tela da camera e fiz aparecer a última imagem da galeria

--- screens/camera.tsx
+++ screens/camera.tsx
@@ -1,50 +1,90 @@
-import { useIsFocused } from '@react-navigation/native';
 import { StackScreenProps } from '@react-navigation/stack';
+import { CameraView, CameraType, useCameraPermissions, Camera } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
+import { Album } from 'expo-media-library';
 import { useState, useEffect, useRef } from 'react';
-import { StyleSheet, View, TouchableOpacity, Text, StatusBar } from 'react-native';
-import {
-  Camera,
-  useCameraPermission,
-  useCameraDevice,
-  useMicrophonePermission,
-} from 'react-native-vision-camera';
+import { StyleSheet, View, TouchableOpacity, Text, StatusBar, Button, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import { RootStackParamList } from '../navigation';
 
 type Props = StackScreenProps<RootStackParamList, 'Camera'>;
 
 const ChatWindow = ({ navigation }: Props) => {
-  const device = useCameraDevice('front');
-  const { hasPermission, requestPermission } = useCameraPermission();
-  const { hasPermission: hasMicPermission, requestPermission: requestMicPermission } =
-    useMicrophonePermission();
-  const [permission, setPermission] = useState<null | boolean>(null);
-  const cameraRef = useRef<Camera>(null);
+  const [facing, setFacing] = useState<CameraType>('front');
+  const [permission, requestPermission] = useCameraPermissions();
+
+  const [albums, setAlbums] = useState<Album[] | null>(null);
+  const [permissionResponse, requestMediaPermission] = MediaLibrary.usePermissions();
+  const [lastPhotoUri, setLastPhotoUri] = useState<string | null>(null);
 
   useEffect(() => {
-    (async () => {
-      const status = await requestPermission();
-      const statusMic = await requestMicPermission();
-
-      if (status && statusMic) setPermission(true);
-    })();
+    getAlbums();
   }, []);
 
-  if (!permission) return <View />;
+  if (!permission) {
+    return <View />;
+  }
 
-  if (!device || device == null) return <View />;
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  async function getAlbums() {
+    if (permissionResponse?.status !== 'granted') {
+      await requestMediaPermission();
+    }
+
+    const fetchedAlbums = await MediaLibrary.getAlbumsAsync({
+      includeSmartAlbums: true,
+    });
+    setAlbums(fetchedAlbums);
+
+    // Buscar a última foto da galeria
+    const assets = await MediaLibrary.getAssetsAsync({
+      first: 1, // Buscar apenas a última foto
+      sortBy: MediaLibrary.SortBy.creationTime,
+      mediaType: MediaLibrary.MediaType.photo,
+    });
+
+    if (assets.assets.length > 0) {
+      setLastPhotoUri(assets.assets[0].uri); // Armazena o URI da última foto
+    }
+  }
+
+  function toggleCameraFacing() {
+    if (facing == 'back') {
+      setFacing('front');
+    } else {
+      setFacing('back');
+    }
+  }
 
   return (
     <View style={styles.container}>
       <StatusBar hidden />
-      <Camera
-        style={StyleSheet.absoluteFill}
-        ref={cameraRef}
-        device={device}
-        isActive
-        audio
-        resizeMode="cover"
-      />
+      <CameraView style={styles.camera} facing={facing}>
+        <View style={styles.screenContainer}>
+          <View style={styles.buttonContainer}>
+            <View style={styles.lastImage}>
+              {lastPhotoUri ? (
+                <Image source={{ uri: lastPhotoUri }} style={styles.lastImage} />
+              ) : (
+                <View style={styles.placeholderImage} />
+              )}
+            </View>
+            <TouchableOpacity style={styles.takePhotoButton} onPress={() => {}} />
+            <TouchableOpacity style={styles.toggleFacingButton} onPress={toggleCameraFacing}>
+              <Icon name="rotate-ccw" size={24} color="#fff" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </CameraView>
     </View>
   );
 };
@@ -52,6 +92,50 @@ const ChatWindow = ({ navigation }: Props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+
+  message: {},
+
+  camera: {
+    flex: 1,
+  },
+  screenContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  buttonContainer: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingBottom: 30,
+    flexDirection: 'row',
+    paddingLeft: '5%',
+    paddingRight: '5%',
+  },
+  takePhotoButton: {
+    width: 80,
+    height: 80,
+    backgroundColor: 'white',
+    borderRadius: 50,
+  },
+  toggleFacingButton: {
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+
+    width: 50,
+    height: 50,
+  },
+  lastImage: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'black',
+    borderRadius: 5,
+  },
+
+  placeholderImage: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'black',
+    borderRadius: 5,
   },
 });
 
